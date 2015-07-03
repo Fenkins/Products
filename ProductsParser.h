3845d640686959f1841c5ab792753d3407d84b96
Anatoly Kovalev <inatoley@gmail.com>
2015-07-03 22:34:33 +0300

    more incoming

--- ProductsParser.h
+++ ProductsParser.h
@@ -5,9 +5,15 @@
 //  Created by Fenkins on 03/07/15.
 //  Copyright (c) 2015 Fenkins. All rights reserved.
 //
+// Creating methods and custom initializers to populate productsArray with data
 
 #import <Foundation/Foundation.h>
 
-@interface ProductsParser : NSObject
+@interface ProductsParser : NSObject <NSXMLParserDelegate>
+
+@property (retain,nonatomic) NSMutableArray *productArray;
+
+-(id) initWithArray:(NSMutableArray *) productArray;
+-(void) parseXMLFile;
 
 @end
--- ProductsParser.m
+++ ProductsParser.m
@@ -8,6 +8,33 @@
 
 #import "ProductsParser.h"
 
+// This will create private properties
+@interface ProductsParser()
+@property NSXMLParser *parser;
+@property NSString *element;
+
+@property NSString *currentProductName;
+@property NSString *currentProductDescription;
+
+@end
+
+
+
 @implementation ProductsParser
+
+-(id) initWithArray:(NSMutableArray *)productArray {
+    self = [super init];
+    if (self) {
+        self.productArray = productArray;
+    }
+    return self;
+}
+
+-(void) parseXMLFile {
+    NSURL *xmlPath = [[NSBundle mainBundle]URLForResource:@"productsList" withExtension:@"xml" ];
+    self.parser = [[NSXMLParser alloc]initWithContentsOfURL:xmlPath];
+    self.parser.delegate = self;
+    [self.parser parse];
+}
 
 @end
