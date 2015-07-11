f647f8fa8588bb87a7bae1d234f0a2403a8e1093
Anatoly Kovalev <inatoley@gmail.com>
2015-07-11 18:36:31 +0300

    all looking good

--- AboutDetailViewController.m
+++ AboutDetailViewController.m
@@ -20,13 +20,26 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
     [self.view setBackgroundColor:[UIColor whiteColor]];
 
     UIBarButtonItem *backButton = [[UIBarButtonItem alloc]initWithTitle:@"back" style:UIBarButtonItemStyleDone target:self action:@selector(backButtonPressed)];
 
     self.navigationItem.leftBarButtonItem = backButton;
     [backButton release];
+    
+    CGRect screenRect = [[UIScreen mainScreen]bounds];
+    UILabel *detailedInfoLabel = [[UILabel alloc] initWithFrame:CGRectMake(0.0, 0.0, screenRect.size.width*8/10, screenRect.size.height*8/10)];
+    detailedInfoLabel.center = CGPointMake(screenRect.size.width/2, screenRect.size.height/2+screenRect.size.height/16);
+    detailedInfoLabel.text = NSLocalizedString(@"We supply all kind of boxes, you name it - we got it. \n\n Call us on 1-800-MYBOX \n\n Or visit our website: \n http://profBoxSuppliers.com \n\n You could also visit our office at 30 E University Ave, Gainesville, FL 32601, United States", @"detailed info text");
+    [detailedInfoLabel setTextAlignment:NSTextAlignmentCenter];
+    [detailedInfoLabel setNumberOfLines:20];
+    [self.view addSubview:detailedInfoLabel];
+    
+    UIImage *aboutUsBoxImg = [UIImage imageNamed:@"about_us_detail_Box.png"];
+    UIImageView *aboutUsBoxImgView = [[UIImageView alloc] initWithImage:aboutUsBoxImg];
+    aboutUsBoxImgView.frame = CGRectMake(0.0, 0.0, screenRect.size.width/5, screenRect.size.width/5);
+    aboutUsBoxImgView.center = CGPointMake(screenRect.size.width*4/10, screenRect.size.height/10);
+    [detailedInfoLabel addSubview:aboutUsBoxImgView];
 }
 
 - (void)didReceiveMemoryWarning {
